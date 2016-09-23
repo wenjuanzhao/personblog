b4f47dc92faa80003eff5e2f927023acbcdf55a9
wenjuanzhao <1051743154@.com>
2016-09-23 21:39:02 +0800

    personblogstart

---
+++ models/db.js
@@ -0,0 +1,10 @@
+//封装的是数据库的连接
+var mongodb=require('mongodb'),
+    setting=require('../setting')   //得到数据库的相关配置信息
+var Db=mongodb.Db;
+var connection=mongodb.Connection;
+var Server=mongodb.Server;
+//新建的连接参数为    连接的数据库   服务器
+module.exports=new Db(setting.db,new Server(setting.host,setting.port),{safe:true});
+//将这个连接暴露供其它的文件使用
+
---
+++ setting.js
@@ -0,0 +1,7 @@
+//封装的是一个数据库的相关信息
+module.exports={
+    cookieSecret:'myblog',
+    db:'newblog',
+    host:'localhost',
+    port:'27017'
+}
